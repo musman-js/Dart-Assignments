b8f67f2789d715e19bcbffdb0876e352459d51d1
musman-js <usman2037966@gmail.com>
2024-02-29 16:19:38 +0500

    Flutter Assignment

---
+++ Assignment-2/main.dart
@@ -0,0 +1,49 @@
+main() {
+// ==========Q1==========
+// int breadth = 5;
+// int lenght = 5;
+
+// if(lenght == breadth){
+//   print('Square');
+// }
+// else{
+//    print('Rectangle');
+// }
+
+// ==========Q2==========
+
+// int age1 = 17;
+// int age2 = 16;
+
+// if(age1>age2){
+//   print("Youngest");
+// }
+// else{
+//   print("Smaller");
+// }
+
+// ==========Q3==========
+// num classes = 16;
+// num attendedClass = 10;
+// num percentage = (attendedClass/classes)*100;
+// print(percentage);
+
+// if(percentage>=75){
+//   print("Student is allowed ");
+// }
+// else{
+//   print("student is not allowed");
+// }
+
+// ==========Q4==========
+// num year = 2025;
+// num LeapYear = year % 4 ;
+// print(LeapYear);
+// if(LeapYear == 0){
+//   print("it is a leap year");
+// }
+// else{
+//   print("it is not a leap year");
+// }
+
+}
