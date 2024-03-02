9bc70c56b8c60064d6f4fea07faa13dce3626fdc
mabdullahjs <mabdullah2037@outlook.com>
2024-03-02 19:04:06 +0500

    Assignment 1

---
+++ Assignment-1/main.dart
@@ -0,0 +1,43 @@
+
+
+main(){
+
+// =====Q1===
+// int length = 5;
+// int breadth  = 7;
+// num result = length*breadth;
+// print(result);
+
+
+
+// =====Q2===
+// int num1 =7;
+// num add  = (num1 + 8) / 3;
+// num modulus = add%5;
+// num result = modulus*5;
+// print(result);
+
+ 
+// =====Q3===
+// int num1  = 5;
+// int num2 = 10;
+// if(num1 < 50 && num1<num2){
+//   print('true');
+// }
+// else{
+//   print("False");
+// }
+
+
+// =====Q4===
+// var name = "Robert";
+// int sub1 = 78;
+// int sub2 = 45;
+// int sub3 = 62;
+
+// num total = sub1+sub2+sub3;
+// num percentage = (total/300)*100;
+
+// print("`${name} has total marks ${total} and percentage ${percentage}`");
+
+}
