88ba540627ddc0329832f77aa0dde0e07c325fce
musman-js <usman2037966@gmail.com>
2024-03-08 22:59:24 +0500

    Assignment-3

---
+++ Assignment-3/main.dart
@@ -0,0 +1,31 @@
+main(){
+   
+  //  ======Q1=====
+  // num userValue = -10;
+  // if(userValue >= 1){
+  //   print("It is a postive number");
+  // }
+
+  // else if(userValue == 0){
+  //   print("It zero");
+  // }
+
+  // else if(userValue<=1){
+  //   print("It is a negative number");
+  // }
+
+
+  //  ======Q3=====
+//  num year = 2025;
+// num LeapYear = year % 4 ;
+// print(LeapYear);
+// if(LeapYear == 0){
+//   print("it is a leap year");
+// }
+// else{
+//   print("it is not a leap year");
+// }
+
+
+   
+}
