17bd98f86f215912df129f16e3f91f1c6858f88d
musman-js <usman2037966@gmail.com>
2024-03-09 19:34:53 +0500

    Assignment-4

---
+++ Assignment-4/main.dart
@@ -0,0 +1,53 @@
+void main(){
+
+  // =====Q1====
+  // List name = ['Usman',"ammar","Ayaan","konain"];
+  // print(name);
+
+
+  // =====Q2====
+  // List days = [];
+  // days.add("Monday");
+  // days.add("Tuesday");
+  // days.add("Wednesday");
+  // days.add("Thursday");
+  // days.add("Friday");
+  // days.add("Saturday");
+  // days.add("Sunday");
+  //  print(days);
+
+
+
+  // =====Q3====
+  // List days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+  // days.remove("Sunday");
+  // days.remove("Saturday");
+  // days.remove("Friday");
+  // days.remove("Thursday");
+  // days.remove("Wednesday");
+  // days.remove("Tuesday");
+  // days.remove("Monday");
+  // print(days);
+
+
+
+
+  // =====Q4====
+  // List numbers  = [1,2,3,4,5,6];
+//   var usernumbers = 6;
+//   if(usernumbers== numbers[5]){
+//     print("It is the greatest number");
+//   }
+//   else if(usernumbers == numbers[0]){
+//     print("It is the smallest number");
+//   }
+// else{
+//   print("Ye bech ke numbers he");
+// }
+
+
+  // =====Q5====
+  
+
+
+}
