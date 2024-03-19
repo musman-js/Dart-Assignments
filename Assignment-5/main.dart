14fc1347fdeac6c5945313eb3f9b812ed6097d84
musman-js <usman2037966@gmail.com>
2024-03-19 14:24:14 +0500

    Assignment-5

---
+++ Assignment-5/main.dart
@@ -0,0 +1,78 @@
+main(){
+
+  //===Q1===
+  // List fruits = ["Apple","Banana","Orange"];
+  // print(fruits);
+
+
+   //===Q2===
+  //  List number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+  //  print(number);
+
+
+
+  //===Q3===
+  // List numbers = [1,2,3,4,5,5,3,2];
+  // Set uniqueNumbers = numbers.toSet();
+  // print(uniqueNumbers.toList());
+
+
+
+
+  //===Q4===
+  //  List check = ["ammar ","usman"];
+  //  print(check.isEmpty);
+
+
+
+  //===Q5===
+//  List num1 = [1,2,3,4,5];
+//  List num2 = [6,7,8,9,10];
+//  var merge = num1+num2;
+//  print(merge);
+
+
+
+
+  //===Q6===
+  // Map studentInfo = {
+  //   "name": "Muhammad Usman Khan",
+  //   "age" : 17,
+  //   "Grade" : "A+"
+  // };
+
+  // print(studentInfo['name']);
+  // print(studentInfo['age']);
+  // print(studentInfo['Grade']);
+
+
+
+  //===Q7===
+  // Map karachi={
+  //   "population":2500
+  // };
+  // Map Islamabad =  {
+  //   "population":1500,
+  // };
+  // Map lahore = {
+  //   "population": 10000
+  // };
+
+  // int gettingkarachipop = karachi["population"];
+  // int gettingIslamabadpop = Islamabad["population"];
+  // int gettingLahorepop = lahore["population"];
+
+  // if(gettingkarachipop > gettingIslamabadpop && gettingkarachipop > gettingLahorepop){
+  //   print("`Karachi has largest population ${gettingkarachipop}`");
+  // }
+
+  // else if(gettingIslamabadpop>gettingLahorepop && gettingIslamabadpop > gettingkarachipop){
+  //   print("`Islamabad has largest population ${gettingIslamabadpop}`");
+  // }
+  
+  // else if(gettingLahorepop > gettingkarachipop && gettingLahorepop > gettingIslamabadpop){
+   
+  //   print("`Lahore has largest population ${gettingLahorepop}`");
+  // }
+
+}
