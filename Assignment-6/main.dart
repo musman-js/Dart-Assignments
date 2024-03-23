d9f40a005ce2021e769c47f1bb5023af67c755a4
musman-js <usman2037966@gmail.com>
2024-03-23 18:14:45 +0500

    Assignment-6

---
+++ Assignment-6/main.dart
@@ -0,0 +1,46 @@
+main(){
+
+  //Q1
+  // List name = ['Usman',"ammar","ayaan"];
+  // print(name);
+
+  //Q2
+  // List<String> days = [];
+  // days.addAll(["Monday","Tuesday","Wedensday","Thursday","Friday","Saturday","Sunday"]);
+  // print(days);
+
+
+  //Q3
+  // List days = ["Monday","Tuesday","Wedensday","Thursday","Friday","Saturday","Sunday"];
+  // days.remove("Sunday");
+  // days.remove("Saturday");
+  // days.remove("Friday");
+  // days.remove("Thursday");
+  // days.remove("Wedensday");
+  // days.remove("Tuesday");
+  // days.remove("Monday");
+  // print(days);
+
+
+  //Q4
+  // List numbers = [0,1,2,3,4,5,6,7];
+  //  int greaterNumber = numbers.length;
+  //  print(greaterNumber);
+  //  int smallNumber =numbers[0];
+  //  print(smallNumber);
+
+
+  //Q5
+  // Map name = {
+  //   "name":"usman",
+  //    "age":16,
+  //    "field":"computer science"
+  // };
+
+  // print(name["name"]);
+  // print(name["age"]);
+  // print(name["field"]);
+
+  
+  
+}
