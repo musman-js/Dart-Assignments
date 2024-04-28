95dffb43aae0931bfe874e31737c5e09f33710db
musman-js <usman2037966@gmail.com>
2024-04-29 01:23:41 +0500

    Assignment-7

---
+++ Assignment-7/main.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+main() {
+//Q2
+// var arr = [1,2,4,5,3,10,20,45,33];
+// arr.sort();
+// print(arr);
+// var largestNumber = arr.last;
+// print(largestNumber);
+
+
+
+//Q3
+// var tableNum = 5;;
+// for(var i = 1; i<=10;i++){
+//   print('$tableNum x $i = ${i*tableNum}');
+// }
+
+
+
+//Q4
+  // stdout.write("Enter a word :");
+  // var word = stdin.readLineSync();
+  // var flag = false;
+  // if(word != null){
+  //     var check = word.split('').reversed.join();
+  //     if(check == word){
+  //       flag = true;
+  //     }
+
+  //     if(flag){
+  //       print("It is a Palindrome");
+  //     }
+  //     else{
+  //       print("It is not a Palindrome");
+  //     }
+  // }
+
+
+
+  //Q5
+    // var user = 5;
+    // for(var i = 1; i<= user; i++){
+    //    print('${i.toString()*i}');
+    // }
+
+
+
+  //Q6
+
+
+}
