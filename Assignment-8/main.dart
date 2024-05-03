8ad14a9b1bef188d5720c36f850a258097ec5026
musman-js <usman2037966@gmail.com>
2024-05-03 22:19:52 +0500

    Assignment-8

---
+++ Assignment-8/main.dart
@@ -0,0 +1,115 @@
+import 'dart:io';
+ main(){
+  
+
+  //Q1
+  // stdout.write("Enter a  number: ");
+  // var input = stdin.readLineSync()!;
+  // int digitCount = 0;
+  // var number  = int.parse(input);
+  
+  // if (number == 0) {
+  //   digitCount = 1;
+  // } else {
+  //   while (number != 0) {
+  //     number ~/= 10;
+  //     digitCount++;
+  //   }
+  // }
+  // print("Number of digits: $digitCount");
+
+
+
+
+
+  //Q2
+  // stdout.write("Enter length to generate password : ");
+  // var userlength = stdin.readLineSync()!;
+  // var number = int.parse(userlength);
+  // var password = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  // var length = number;
+  // print(length);
+  
+  // if(number == 0){
+  //   print("Sorry the length mush be 1");
+  // }
+  // else{
+  //    while(number != 0){
+  //       print(password.length);
+  //    }
+  
+  // }
+
+
+
+  //Q3
+  // stdout.write("Enter number");
+  // var number = stdin.readLineSync()!;
+  // var tableNum = int.parse(number);
+ 
+  // var i = 0;
+  // while (i <= 9) {
+  //   i++;
+  //   print('$number x $i = ${i*tableNum}');
+  // }
+
+
+  //Q4
+  // var i=0;
+  // var sum = 0;
+  // do {
+  //   i++;
+  //     if(i%2 != 0){
+  //        sum+= i;
+  //   }
+  // } while (i<=49);
+
+  // print(sum);
+
+
+
+
+  //Q5
+  // stdout.write("Enter number to check");
+  // var numeber = stdin.readLineSync()!;
+  // var userNumber = int.parse(numeber);
+
+  // if(userNumber > 0){
+  //   print("it is a postive number");
+  // }
+  // else if(userNumber < 0){
+  //   print("It is a negative number");
+  // }
+  // else if(userNumber == 0){
+  //   print("It is a zero");
+  // }
+
+
+
+  
+  //Q6
+  // var number = 5;
+  // var factorial = 1;
+  // var i = 1;
+  
+  // while (i<=number) {
+  //   factorial *= i;
+  //    i++;
+  // }
+
+  // print(factorial);
+
+
+
+
+
+
+  //Q12
+  // var i = 1;
+  // do {
+  //   print(i*2);   
+  //   i++;
+  // } while (i<=10);
+ 
+
+ }
